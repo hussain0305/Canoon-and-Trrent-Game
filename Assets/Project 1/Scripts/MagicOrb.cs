ba1a39ba9aad77cddd356182787e67eb6c1b807e
hussain0305 <rulerofevil@gmail.com>
2017-05-04 14:36:40 -0700

    Initial Commit
    Initial Commit

---
+++ Assets/Project 1/Scripts/MagicOrb.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicOrb : MonoBehaviour
+{
+	public int hitpoint = 10;
+	public float speed = 5.0f;
+	public AudioClip audioHit = null;
+	public AudioClip audioShoot = null;
+	public ParticleSystem particle = null;
+
+	private bool canMove = true;
+
+
+	void Awake ()
+	{
+		this.GetComponent<AudioSource>().PlayOneShot (audioShoot);
+	}
+
+	void Update ()
+	{
+		MoveObject ();
+	}
+
+	void MoveObject ()
+	{
+		if ( canMove )
+		{
+			this.transform.Translate (speed * Time.deltaTime, 0, 0);
+		}
+	}
+
+	void OnTriggerEnter ( Collider other )
+	{
+		this.GetComponent<AudioSource>().PlayOneShot (audioHit);
+
+		this.GetComponent<Renderer>().enabled = false;
+		this.GetComponent<Collider>().enabled = false;
+
+		canMove = false;
+
+		particle.enableEmission = false;
+
+		Destroy (this.gameObject, audioHit.length);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
